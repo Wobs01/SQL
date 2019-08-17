6524170efc6a1b65d487189884df4d784bf4c395
wobs01 <robin@rverhoeven.nl>
2019-08-17 11:11:23 +0200

    Git init

---
+++ SQLmgmt.psm1
@@ -0,0 +1,265 @@
+
+function add-toSQLtable {
+    <#   
+    .SYNOPSIS   
+    Function to convert a system object to a SQL query and insert it into the table
+        
+    .DESCRIPTION 
+    Function to convert a system object to a SQL query and insert it into the table
+
+    .NOTES	
+        Author: Robin Verhoeven
+        Requestor: -
+        Created: -
+        
+        
+
+    .LINK
+        https://github.com/Wobs01/SQL
+
+    .EXAMPLE   
+    . add-toSQLtable -Connection $connection -tablename "Testtable" -object "$SO"
+    
+
+    #>
+    [Cmdletbinding()] 
+    param(
+        [parameter(Mandatory = $true)]
+        $connection,
+        [parameter(Mandatory = $true)]
+        $tablename,
+        [parameter(Mandatory = $true)]
+        $object
+              
+    ) 
+
+   
+    $columnnames = ($object| select-object -First 1).psobject.properties.name 
+
+    [string]$columnstring = "([" + ($columnnames -join "],[") + "])"
+    [string]$parametercountstring = "(" + ("@" + ($columnnames -join ",@")) + ")"
+    $SQLinsert = @("INSERT INTO $tablename " + $columnstring + " VALUES " + $parametercountstring + ";") 
+    
+    $command = $connection.CreateCommand()
+    
+    foreach ($Item in $object) {
+        
+        try {
+            $command.Parameters.Clear()
+            $command.CommandText = $SQLinsert
+            
+            foreach ($columnname in $columnnames) {
+                if ([string]::IsNullOrEmpty($Item.$columnname)) {
+                    $Item.$columnname = "NULL"
+                }
+                [void]$command.Parameters.Add("@$columnname", $Item.$columnname)
+                
+            
+            }
+                    
+            [void]$command.ExecuteNonQuery() 
+    
+        }
+        
+        catch {
+            throw $Error[0]
+        }
+    }
+   
+
+}
+
+function add-toSQLtablebulk {
+    [Cmdletbinding()] 
+    param([parameter(Mandatory = $false)]$connection,
+        [parameter(Mandatory = $false)]$tablename,
+        [parameter(Mandatory = $false)]$SO
+              
+    ) 
+    try {
+        $bulk = new-object ("System.Data.SqlClient.SqlBulkCopy") $connection
+        $bulk.DestinationTableName = $tablename
+        $datatable = add-datatable -inputobject $SO
+        $bulk.WriteToServer($datatable) #| Out-Null
+    }
+    catch {
+        $Error[0] | Out-Host
+    }
+    
+}
+
+function remove-SQLtable {
+    [Cmdletbinding()] 
+    param([parameter(Mandatory = $false)]$connection,
+        [parameter(Mandatory = $false)]$tablename
+              
+              
+    ) 
+    try {
+        $command = $connection.CreateCommand()        
+        $command.CommandText = "drop table $tablename"
+                  
+      
+        [void]$command.ExecuteNonQuery()
+            
+    }
+        
+    catch {
+        $Error[0] | Out-Host
+    }
+
+}
+
+function new-SQLtable {
+    [Cmdletbinding()] 
+    param([parameter(Mandatory = $false)]$connection,
+        [parameter(Mandatory = $false)]$tablename,
+        [parameter(Mandatory = $false)]$SO
+              
+    ) 
+    try {
+        $command = $connection.CreateCommand()
+        
+        $command.CommandText = "Select * from $tablename" 
+        $command.Parameters.Clear()
+        $reader = $command.ExecuteReader()           
+        $reader.Close()
+    }
+    catch [System.Management.Automation.MethodInvocationException] {
+        
+            
+        $columnnames = ($SO| select-object -First 1).psobject.properties.name 
+        $columntypes = ($SO| select-object -First 1).psobject.properties.TypeNameOfValue
+        $i = 0
+        [string]$SQLcreate = "CREATE TABLE " + $tablename + " ("
+        foreach ($columnname in $columnnames) {
+                
+            $columntype = $columntypes[$i].split("\.")[-1]
+                
+            switch ($true) {
+                ($columntype -eq "Datetime") {
+                    $SQLcolumntype = "DATETIME" 
+                }
+                ($columntype -eq "GUID") {
+                    $SQLcolumntype = "UNIQUEIDENTIFIER"
+                }
+                ($columntype -eq "INT32") {
+                    $SQLcolumntype = "INTEGER"
+                }
+                default {
+                    $SQLcolumntype = "VARCHAR(MAX)" 
+                }
+            }
+            $SQLcreate = $SQLcreate + "[" + $columnname + "] " + $SQLcolumntype + ","
+            $I++
+                       
+        }  
+        $SQLcreate = ($SQLcreate -replace ".$") + ");"   
+        try {
+            $command = $connection.CreateCommand()
+                
+            $command.CommandText = $SQLcreate
+            [void]$command.ExecuteNonQuery()
+        }
+        catch {
+            $Error[0] | out-host
+        }
+    }
+    
+}
+
+function open-SQLdatabase {
+    [Cmdletbinding(DefaultParameterSetName = "WindowsAuth")] 
+    param([parameter(Mandatory = $false)]
+        [string]$servername,
+        [parameter(Mandatory = $false)]
+        [string]$instancename,
+        [parameter(Mandatory = $false)]
+        [string]$databasename,
+        [parameter(Mandatory = $false,ParameterSetName='WindowsAuth')]
+        [switch]$usewindowsauthentication,
+        [parameter (Mandatory = $false,ParameterSetName='SQLAuth')]
+        [string]$username,
+        [parameter (Mandatory = $false,ParameterSetName='SQLAuth')]
+        [string]$password
+
+              
+    ) 
+    if ($instancename) {
+        $serverstring = "$servername\$instancename"
+    }
+    else {
+        $serverstring = $servername
+    }
+
+    try {
+        $connection = New-Object System.Data.SqlClient.SqlConnection
+        $connection.ConnectionString = "Server = $serverstring ; Database = $databasename; Integrated Security = True;" 
+        
+        $connection.Open()
+        "Successfully connected to $serverstring"  | Out-Host
+    }
+    catch {
+        $Error[0] | Out-Host
+        $connection = $null
+        "failed to connect to $serverstring"  | Out-Host
+    }
+
+    return $connection
+}
+
+function close-SQLdatabase {
+    [Cmdletbinding()] 
+    param([parameter(Mandatory = $false)]$connection            
+              
+    ) 
+    try {
+        $connection.Close()
+        $connection.Dispose()
+        "Closed connection " + $connection.DataSource | Out-Host
+    }
+    catch {
+        $Error[0] | write-log -type Error
+        "Failed to close connection to " + $connection.DataSource | write-log -type error
+    }
+}
+
+function get-SQLtableinfo {
+    [Cmdletbinding()] 
+    param([parameter(Mandatory = $false)]$connection,
+        [parameter(Mandatory = $false)]$tablename           
+              
+    ) 
+    try {
+        $command = $connection.createcommand()      
+        
+        $command.CommandText = "Select * from $tablename" 
+        $command.Parameters.Clear()
+        $reader = $command.ExecuteReader()
+        $result = New-Object System.Data.DataTable
+        $result.Load($reader)       
+        $reader.Close()
+    }
+    catch {
+        $Error[0]
+    }
+    
+    return $result
+
+}
+
+function add-datatable ($inputobject) {
+    $datatable = new-object System.data.datatable
+    $columnnames = ($inputobject | select-object -First 1).psobject.properties.name 
+    $columnnames | foreach-object {$datatable.Columns.Add($_)} | Out-Null
+    foreach ($object in $inputobject) {        
+        $row = $datatable.NewRow()
+        foreach ($column in $columnnames) {
+            $row.item($column) = $object.$column
+        }
+        $datatable.Rows.Add($row)
+    }
+    return , $datatable
+}
+
+Export-ModuleMember -Function * -Alias *
---
+++ import-csvtoSQL.ps1
@@ -0,0 +1,43 @@
+﻿[Cmdletbinding(DefaultParameterSetName = 'file')] 
+param(
+    [parameter(Mandatory = $true, ParameterSetName = 'File')]$sourcefile,
+    [parameter(Mandatory = $false)]$SQLtable = "SQLauditinfo",
+    [parameter(Mandatory = $false)]$SQLdatabase = "Auditlog",
+    [parameter(Mandatory = $false)]$SQLinstancename  = "SQLmgmt_prd"
+    
+)
+
+
+
+$script:timestamp = Get-Date
+$SQLmodule = "$PSScriptRoot\SQLmgmt.psm1"
+
+
+try {
+    $CSV = Import-Csv -Path $sourcefile -ErrorAction Stop     
+    }
+catch {
+    $Error[0].exception | out-host
+    "Unable to open $($sourcefile)" |  out-host 
+    $CSV = $null   
+}
+
+try {
+    Import-Module $SQLmodule -ErrorAction Stop
+    $connection = open-SQLdatabase -servername $env:COMPUTERNAME -instancename $SQLinstancename -databasename $SQLdatabase
+    }
+catch {
+    $connection = $null
+    "SQL connection not available" | Out-Host
+}
+$i2 = 1
+foreach ($line in $csv) {
+    Write-Progress -Activity "processing $i2 of $($csv.count)" -PercentComplete (($i2/$csv.Count) * 100)
+    if ($i2 -le 1) {
+        new-SQLtable -tablename $SQLtable -connection $connection -SO $line        
+    }
+    $line.timestamp = [datetime]$line.timestamp
+    $i2++
+    add-toSQLtable -connection $connection -tablename $SQLtable -SO $line
+}
+close-SQLdatabase -connection $connection
